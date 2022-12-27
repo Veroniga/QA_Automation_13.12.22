b6d177c261b4dc7e4b666b8913ead6682161db78
Veronika Genkinova <veranika-2005@ukr.net>
2022-12-27 17:21:47 +0200

    added files

---
+++ src/main/java/com/hillel/lesson2/Homework2.java
@@ -0,0 +1,17 @@
+package com.hillel.lesson2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        double northСoordinate = 46.485556;
+        double eastСoordinate = 30.741667;
+        char degreeSymbol = '\u00BA';
+        System.out.println("Odesa Opera and Ballet Theatre");
+        System.out.println("North: " + northСoordinate + degreeSymbol);
+        System.out.println("East: " + eastСoordinate + degreeSymbol);
+
+    }
+}
+
+
+
---
+++ src/main/java/com/hillel/lessons/Homework1.java
@@ -0,0 +1,7 @@
+package com.hillel.lessons;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
