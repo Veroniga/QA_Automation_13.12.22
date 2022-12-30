d4d20787dc69f6f1142411118b4a8d0173ae867e
Veronika Genkinova <veranika-2005@ukr.net>
2022-12-30 18:30:46 +0200

    add another line

--- src/main/java/com/hillel/lessons/Homework1.java
+++ src/main/java/com/hillel/lessons/Homework1.java
@@ -3,5 +3,6 @@ package com.hillel.lessons;
 public class Homework1 {
     public static void main(String[] args) {
         System.out.print("Hello world");
+        System.out.print("Hello world");
     }
 }
