b45c242f2041cd371b05c27359998860c4b63554
Veronika Genkinova <veranika-2005@ukr.net>
2023-01-01 21:51:42 +0200

    Homework5 added

---
+++ src/main/java/com/hillel/lesson5/Homework5.java
@@ -0,0 +1,17 @@
+package com.hillel.lesson5;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        int c = 11;
+
+        int volume = a * b * c;
+        System.out.println(volume);
+
+        int lenth = 4 * (a + b + c);
+        System.out.println(lenth);
+
+
+    }
+}
