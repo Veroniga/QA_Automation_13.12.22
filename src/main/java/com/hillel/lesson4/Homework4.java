e981afb0fc3170d7c5a88bd2924d2b9b5b082939
Veronika Genkinova <veranika-2005@ukr.net>
2023-01-03 13:27:47 +0200

    Homework4 edited

--- src/main/java/com/hillel/lesson4/Homework4.java
+++ src/main/java/com/hillel/lesson4/Homework4.java
@@ -2,16 +2,15 @@ package com.hillel.lesson4;
 
 public class Homework4 {
     public static void main(String[] args) {
-    int a = 7;
-    int b = 9;
-    int c = 11;
+    int parallelepipedSide1 = 7;
+    int parallelepipedSide2 = 9;
+    int parallelepipedSide3 = 11;
 
-    int volume = a * b * c;
-    System.out.println(volume);
+    int volume = parallelepipedSide1 * parallelepipedSide2 * parallelepipedSide3;
+    System.out.println("Об'єм паралелепіпеда = " + volume);
 
-    int length = 4 * (a + b + c);
-    System.out.println(length);
-
+    int length = 4 * (parallelepipedSide1 + parallelepipedSide2 + parallelepipedSide3);
+    System.out.println("Довжина всіх сторін паралелепіпеда = " + length);
 
     }
 }
