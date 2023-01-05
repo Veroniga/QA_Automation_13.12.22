e90fe77d05bb96e8cfa3a11dce179e9e43e39f57
Veronika Genkinova <veranika-2005@ukr.net>
2023-01-05 14:04:41 +0200

    Homework6 edited

--- src/main/java/com/hillel/lesson5/Homework6.java
+++ src/main/java/com/hillel/lesson5/Homework6.java
@@ -5,89 +5,118 @@ import java.util.Scanner;
 public class Homework6 {
     public static void main(String[] args) {
 
+        int team1Player1 = 0;
+        int team1Player2 = 0;
+        int team1Player3 = 0;
+        int team1Player4 = 0;
+        int team1Player5 = 0;
+        int team2Player1 = 0;
+        int team2Player2 = 0;
+        int team2Player3 = 0;
+        int team2Player4 = 0;
+        int team2Player5 = 0;
+
         System.out.println("Введіть ім'я першої команди");
         Scanner scanner = new Scanner(System.in);
         String teamName1 = scanner.nextLine();
 
         System.out.println("Введіть кількість фрагів для гравців першої команди");
 
-
-
         if (scanner.hasNextInt()){
-            int team1player1 = scanner.nextInt();
+            team1Player1 = scanner.nextInt();
             scanner.nextLine();
         } else {
             System.out.println("Невірне значення кількості фрагів");
-            System.exit(status: 0);
+            System.exit(0);
         }
 
         if (scanner.hasNextInt()){
-            int team1player2 = scanner.nextInt();
+            team1Player2 = scanner.nextInt();
             scanner.nextLine();
         } else {
             System.out.println("Невірне значення кількості фрагів");
-            //System.exit(status: 0);
+            System.exit(0);
         }
 
         if (scanner.hasNextInt()){
-            int team1player3 = scanner.nextInt();
+            team1Player3 = scanner.nextInt();
             scanner.nextLine();
         } else {
             System.out.println("Невірне значення кількості фрагів");
-            //System.exit(status: 0);
+            System.exit(0);
         }
 
         if (scanner.hasNextInt()){
-            int team1player4 = scanner.nextInt();
+            team1Player4 = scanner.nextInt();
             scanner.nextLine();
         } else {
             System.out.println("Невірне значення кількості фрагів");
-            //System.exit(status: 0);
+            System.exit(0);
         }
 
         if (scanner.hasNextInt()){
-            int team1player5 = scanner.nextInt();
+            team1Player5 = scanner.nextInt();
             scanner.nextLine();
         } else {
             System.out.println("Невірне значення кількості фрагів");
-            //System.exit(status: 0);
+            System.exit(0);
         }
 
-        //System.out.println(team1player5);
-
-
-        /* System.out.println("Введіть ім'я другої команди");
+        System.out.println("Введіть ім'я другої команди");
         String teamName2 = scanner.nextLine();
-        System.out.println(teamName2);
 
         System.out.println("Введіть кількість фрагів для гравців другої команди");
 
-        Scanner scanner = new Scanner(System.in);
-        int team1player1 = scanner.nextInt();
+        if (scanner.hasNextInt()){
+            team2Player1 = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Невірне значення кількості фрагів");
+            System.exit(0);
+        }
 
-        Scanner scanner3 = new Scanner(System.in);
-        int team1player3 = scanner3.nextInt();
+        if (scanner.hasNextInt()){
+            team2Player2 = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Невірне значення кількості фрагів");
+            System.exit(0);
+        }
 
-        Scanner scanner4 = new Scanner(System.in);
-        int team1player4 = scanner4.nextInt();
-        Scanner scanner5 = new Scanner(System.in);
-        int team1player5 = scanner5.nextInt();
+        if (scanner.hasNextInt()){
+            team2Player3 = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Невірне значення кількості фрагів");
+            System.exit(0);
+        }
 
-        System.out.println(team1player5);*/
+        if (scanner.hasNextInt()){
+            team2Player4 = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Невірне значення кількості фрагів");
+            System.exit(0);
+        }
 
-//
-//
+        if (scanner.hasNextInt()){
+            team2Player5 = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Невірне значення кількості фрагів");
+            System.exit(0);
+        }
 
-       // int team1Score = (team1Player1 + team1Player2 + team1Player3 + team1Player4 + team1Player5) / 5;
-//        int team2Score = (team2Player1 + team2Player2 + team2Player3 + team2Player4 + team2Player5) / 5;
-//
-//    if (team1Score > team2Score){
-//        System.out.println("Перемогла команда" + teamName1 + "набрала" + team1Score + "очків");
-//    } else if (team1Score < team2Score) {
-//        System.out.println("Перемогла команда" + teamName2 + "набрала" + team2Score + "очків");
-//    } else {
-//        System.out.println("Перемогла команда дружба!");
-//    }
+        int team1Score = (team1Player1 + team1Player2 + team1Player3 + team1Player4 + team1Player5) / 5;
+        int team2Score = (team2Player1 + team2Player2 + team2Player3 + team2Player4 + team2Player5) / 5;
+
+    if (team1Score > team2Score){
+        System.out.println("Перемогла команда " + teamName1 + " набравши " + team1Score + " очків");
+    } else if (team1Score < team2Score) {
+        System.out.println("Перемогла команда " + teamName2 + " набравши " + team2Score + " очків");
+    } else {
+        System.out.println("Рахунок рівний, перемогла дружба!");
+    }
 
 
     }
