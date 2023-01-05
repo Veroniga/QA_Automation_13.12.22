b319272c78990b4a986bc45aadd6830e3bfcd381
Veronika Genkinova <veranika-2005@ukr.net>
2023-01-05 21:57:42 +0200

    Homework7 edited

--- src/main/java/com/hillel/lesson6/Homework7.java
+++ src/main/java/com/hillel/lesson6/Homework7.java
@@ -1,4 +1,30 @@
 package com.hillel.lesson6;
 
+import java.util.Scanner;
+
+//Зробити гру, в якій потрібно відгадати ціле число, яке "загадав" комп'ютер у діапазоні від 0 до 10
+//Користувач повинен мати можливість вгадувати в кілька спроб (мінімум 3).
+
 public class Homework7 {
-}
+    public static void main(String[] args) {
+
+        int randomNumber = (int) (Math.random() * 10);
+        System.out.println (randomNumber);
+        System.out.println("Спробуйте відгадати ціле число, яке \"загадав\" комп'ютер у діапазоні від 0 до 10");
+
+        for (int i = 1; i <=3; i++){
+            Scanner scanner = new Scanner(System.in);
+            int turn = scanner.nextInt();
+            scanner.nextLine();
+                if (turn > randomNumber){
+                    System.out.println("Спробуйте обрати менше число");
+                }
+                else if (turn < randomNumber){
+                    System.out.println("Спробуйте обрати більше число");
+                }
+                else {
+                    System.out.println("Вітаємо! Це вірна відповідь!");
+                }
+            }
+        }
+    }
