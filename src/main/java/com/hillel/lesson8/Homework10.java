ea130dbf6003121c3c1b15f034f00442997d4723
Veronika Genkinova <veranika-2005@ukr.net>
2023-01-17 18:49:45 +0200

    Homework10 edited

--- src/main/java/com/hillel/lesson8/Homework10.java
+++ src/main/java/com/hillel/lesson8/Homework10.java
@@ -9,21 +9,20 @@ public class Homework10 {
 
         int m = 0;
         int n = 0;
-        //int [][] array1 = new int [][];
-       // int [][] array2 = new int [][];
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number of rows");
+        System.out.println("Enter number of rows:");
         if(scanner.hasNextInt()) {
                 m = scanner.nextInt();
                 scanner.nextLine();
             }
 
-        System.out.println("Enter number of columns");
+        System.out.println("Enter number of columns:");
         if(scanner.hasNextInt()) {
             n = scanner.nextInt();
             scanner.nextLine();
         }
+        System.out.println();
 
         int [][] array1 = new int [m][n];
 
@@ -33,6 +32,7 @@ public class Homework10 {
             }
         }
 
+        System.out.println("Before:");
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array1[i].length; j++) {
                 System.out.print(array1[i][j] + "\t");
@@ -43,22 +43,20 @@ public class Homework10 {
 
         int [][] array2 = new int [n][m];
 
-
-//        for (int i = 0; i < array.length; i++) {
-//            for (int j = i + 1; j < array[i].length; j++) {
-//                int temp = array[i][j];
-//                array[i][j] = array[j][i];
-//                array[j][i] = temp;
-//            }
-//        }
-//
-//        System.out.println("AFTER");
-//        for (int i = 0; i < array.length; i++) {
-//            for (int j = 0; j < array[i].length; j++) {
-//                System.out.print(array[i][j] + "\t");
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = i; j < array1[i].length; j++) {
+                int temp = array1[i][j];
+                array2[i][j] = array1[j][i];
+                array2[j][i] = temp;
+            }
+        }
+        System.out.println("After:");
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
    }
 }
