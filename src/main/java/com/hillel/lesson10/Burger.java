6372ecfb6966d19911c70d62ac3e337f0ca3a7de
Veronika Genkinova <veranika-2005@ukr.net>
2023-01-24 17:06:11 +0200

    Homework12 edited

--- src/main/java/com/hillel/lesson10/Burger.java
+++ src/main/java/com/hillel/lesson10/Burger.java
@@ -6,12 +6,6 @@ public class Burger {
     String cheese;
     String mayonnaise;
     String greens;
-    void burgerTypes(){
-        System.out.println("Classic Burger consists of: bread, meat, cheese, mayonnaise,greens");
-//        System.out.println("Diet Burger consists of: " + bread + ", " + meat + ", " + cheese + ", " + greens);
-//        System.out.println("Vegan Burger consists of: " + bread + ", " + cheese + ", " + greens);
-
-    }
 
     public Burger (String bread, String meat, String cheese, String mayonnaise, String greens){
         this.bread = bread;
@@ -19,21 +13,20 @@ public class Burger {
         this.cheese = cheese;
         this.mayonnaise = mayonnaise;
         this.greens = greens;
-        System.out.println("Classic Burger consists of: " + bread + ", " + meat + ", " + cheese + ", " + mayonnaise + ", " + greens);
+        System.out.println("Classic Burger consists of " + bread + ", " + meat + ", " + cheese + ", " + mayonnaise + ", " + greens);
     }
      public Burger(String bread, String meat, String cheese, String greens){
         this.bread = bread;
         this.meat = meat;
         this.cheese = cheese;
         this.greens = greens;
-        System.out.println("Diet Burger consists of: " + bread + " " + meat + " " + cheese + " " + greens);
+        System.out.println("Diet Burger consists of " + bread + " " + meat + " " + cheese + " " + greens);
     }
-    /*public Burger (String bread, String mayonnaise, String cheese, String greens){
+    public Burger (String bread, String cheese, String greens){
         this.bread = bread;
         this.cheese = cheese;
-        this.mayonnaise = mayonnaise;
         this.greens = greens;
-        System.out.println("Vegan Burger consists of: " + bread + " " + cheese + " " + greens);*/
+        System.out.println("Vegan Burger consists of " + bread + " " + cheese + " " + greens);
     }
 
 }
--- src/main/java/com/hillel/lesson10/Main.java
+++ src/main/java/com/hillel/lesson10/Main.java
@@ -1,27 +1,11 @@
 package com.hillel.lesson10;
 
-//Зробити клас Burger, який матиме поля:
-//        булочка, м'ясо, сир, зелень, майонез
-
-//        Зробити три конструктори, які дозволятимуть робити бургери трьох видів:
-//        звичайний бургер (усі компоненти)
-//        дієтичний бургер (без майонезу)
-//        вегетеріанський (без мя'са)
-// У кожен конструктор додати виведення в консоль складу даного варіанту бургера
-// (при створенні екземпляра бургера, який використовує даний конструктор - буде виводитися в консоль його склад).
-// Зробити клас BurgerMain, в якому створити три різні бургери.
-// Їхній склад буде автоматично виводитися на екран під час запуску програми.
-
 public class Main {
     public static void main(String[] args) {
 
-        Burger.burgerTypes();
-//        Burger burger = new Burger("bread", "meat", "cheese", "mayonnaise", "greens");
-//        Burger burger = new Burger("bread",);
-
-//        burger.burgerType();
-
+        Burger burger = new Burger("bread", "meat", "cheese", "mayonnaise", "greens");
+        Burger dietBurger = new Burger("bread", "meat", "cheese", "greens");
+        Burger veganBurger = new Burger("bread", "cheese", "greens");
     }
-
 }
 
